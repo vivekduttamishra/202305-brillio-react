553f6ddd58a4905bb06be0df220c4ec19fdabcdd
Satya Pavani Chekka <61940479+pavani2312@users.noreply.github.com>
2023-05-18 14:10:05 +0530

    Update Assignment2_3.js

--- assignments/Assignment2_3.js
+++ assignments/Assignment2_3.js
@@ -18,73 +18,10 @@ function mapFun(matchFun,values){
 
 
 
-// Common Array Functions  
 
-// filter 
-
-// returns a list of matching items 
-
-// same as our search function  
-
-// find 
-
-// like filter, but returns 
-
-// first matching item only 
-
-// returns undefined if no match is found 
- 
-
-// forEach 
-
-// iterates over each item 
-
-// why? 
-
-// can perform some action over each item 
- 
-
-// map 
-
-// returns new array  
-
-// by transforming each item of original array into something different 
-
-// The new array may contain a new set of values derived from the original set of values  
-
-// example 
- 
-
-// var result = books.map( b => b.title.toUpperCase()); 
-
- 
-
-// [  'THE ACCURSED GOD', 'THE SONS OF FORTUNE' , 'मानस', 'SINS OF FATHER' ] 
-
- 
-
-// var result = books.map( b=> b.price /2 ); 
-
- 
-
-// [ 149.5, 225, 99.5, 125] 
-
- 
-
-// var result = book.map( b=>  ( {  title: b.title, pricing: b.price>250?"expensive":"fair" }) ); 
 
  
 
  
 
-// [  
 
-// { title:'The Accursed God', pricing:'expensive'},  
-
-// {title:'The Sons of Fortune', pricing:'expensive'},  
-
-// {title:'मानस', pricing:'fair'},  
-
-// {title: 'Sins of Father', pricing:'fair' }  
-
-// ]  
