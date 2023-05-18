bfae831d8b92b65cac43630feb31e8e7020a15de
satya Pavani Chekka <satyapavanichekka1997@gmail.com>
2023-05-18 14:03:43 +0530

    assignment of 2.2, 2.3, 2.4 by satyapavani_ch

---
+++ assignments/Assignment2_2.js
@@ -0,0 +1,24 @@
+
+function search(match,values){
+    var result=[];
+
+    for(var value of values){
+        if(match(value))
+            result.push(value);        
+    }
+
+    return result;
+}
+
+var books=[
+
+    {title:'The Accursed God', author:'Vivek Dutta Mishra', price:299},
+    {title:'The Sons of Fortune', author:'SJ', price:450},
+    {title:'मानस', author:'Vivek Dutta Mishra', price:199},
+    {title:'Sins of Father', author:'SJ',price:250},
+];
+
+
+function authorBy(value) { 
+    
+}
