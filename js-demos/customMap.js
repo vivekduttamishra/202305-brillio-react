f946d1a4b345e28b59bdeff142eb33630171ea66
Mohasin Kazi <mohasink.24@gmail.com>
2023-05-18 14:36:28 +0530

    Assignement 2

--- js-demos/customMap.js
+++ js-demos/customMap.js
@@ -1,8 +1,44 @@
 function map(object, transformer) {
     let result = [];
     for (let i = 0; i < object.length; i++) {
-        result.push(transformer(x));
+        result.push(transformer(object[i]));
     }
     return result;
-
 }
+
+
+let books = [
+    {
+        "title": "Sometitle",
+        "author": "abc",
+        "price": "Someprice",
+        "rating": "Somerating"
+    },
+    {
+        "title": "MyBook",
+        "author": "Jeffrey Archer",
+        "price": "Someprice",
+        "rating": "Somerating"
+    },
+    {
+        "title": "Sometitle",
+        "author": "vivek",
+        "price": "Someprice",
+        "rating": "Somerating"
+    },
+    {
+        "title": "Sometitle",
+        "author": "vivek sasd",
+        "price": "Someprice",
+        "rating": "Somerating"
+    },
+    {
+        "title": "Sometitle",
+        "author": "vivekanand",
+        "price": "Someprice",
+        "rating": "Somerating"
+    }
+]
+
+result = map(books, (x) => x.title = x.title.toUpperCase());
+console.log(result);
