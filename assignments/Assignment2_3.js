902771ea20c76416d6e0e861ddc7696f8c85c4e1
Satya Pavani Chekka <61940479+pavani2312@users.noreply.github.com>
2023-05-18 14:12:41 +0530

    Update Assignment2_3.js

--- assignments/Assignment2_3.js
+++ assignments/Assignment2_3.js
@@ -3,17 +3,7 @@
 // Write your own map function which works just like built-in map function
 
 // map takes an array and function as argumentfunction 
-function mapFun(matchFun,values){
-    var arrResult=[]
 
-    for(let i=0;i<values.length;i++){
-        arrResult.push(matchFun(values[i]));
-    }
-    return arrResult;
- }
-
- var mapResult=mapFun(num => num ** 2,[2,3,4,5,6])
- console.log("mapping",mapResult);
 
 
 
