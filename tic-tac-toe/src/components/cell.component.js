7c5a232636f7a3866b7584013ecabbf091928e24
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-05-23 18:00:32 +0530

    day#5 update#3

--- tic-tac-toe/src/components/cell.component.js
+++ tic-tac-toe/src/components/cell.component.js
@@ -1,21 +1,42 @@
+import React from 'react';
 
 
-let Cell=({value})=>{
-
-    var cellStyle={
-
-    };
-
-    if(!value){
-        value="_";
-        cellStyle.color="transparent";
+class Cell extends React.Component {
+    state={
+        value:null
     }
 
-    return (
-        <button className='cell' style={cellStyle} >
-           {value}
-        </button>
-    );
+    handleClick=()=>{
+
+        var newValue= this.state.value==='O'?'X':'O';
+
+        this.setState({value:newValue});
+
+        console.log('this.state.value',this.state.value);
+        console.log('newValue',newValue);
+    }
+
+    render(){
+
+        var cellStyle={};
+        
+        if(!this.state.value){        
+            cellStyle.color="transparent";
+        }
+
+        return (
+            <button className="cell" style={cellStyle}
+                onClick={this.handleClick}
+            >
+                {this.state.value??"_"}
+            </button>
+        )
+        
+    }
+
+    
 }
+
+
 
 export default Cell;
