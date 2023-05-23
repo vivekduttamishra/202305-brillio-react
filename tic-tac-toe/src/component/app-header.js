20e5bfd0efe4fee8afa7279aad7dfdc09e1ca351
Mohasin Kazi <mohasink.24@gmail.com>
2023-05-23 15:23:41 +0530

    cell

---
+++ tic-tac-toe/src/component/app-header.js
@@ -0,0 +1,9 @@
+
+function AppHeader() {
+    return (
+      <div>
+          <h1>Tic Tac Toe Game</h1>
+      </div>
+    );
+}
+export default AppHeader();
---
+++ tic-tac-toe/src/components/cell.component.js
@@ -0,0 +1,9 @@
+let Cell = (props) => {
+    return (
+        <div className='status'>
+            <button className='cell'></button>
+        </div>
+    );
+}
+
+export default Cell;
