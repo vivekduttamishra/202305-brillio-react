7e560fbb533ae3d52983663be6b52826d51ab5da
mohasink24 <mohasink.24@gmail.com>
2023-05-24 14:17:33 +0530

    tic tac toe added styling

--- tic-tac-toe/src/components/board.component.js
+++ tic-tac-toe/src/components/board.component.js
@@ -3,13 +3,14 @@ import Cell from "./cell.component";
 let Board = (props) => {
     return (
         <div className='board'>
+            <Cell value="O"/>
+            <Cell/>
+            <Cell/>
+
             <Cell/>
             <Cell/>
             <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
-            <Cell/>
+
             <Cell/>
             <Cell/>
             <Cell/>
--- tic-tac-toe/src/components/cell.component.js
+++ tic-tac-toe/src/components/cell.component.js
@@ -1,9 +1,52 @@
-let Cell = (props) => {
-    return (
-        <div className='status'>
-            <button className='cell'></button>
-        </div>
-    );
+import React from 'react';
+
+class Cell extends React.Component {
+
+    state = {
+        value: null
+    }
+
+    render() {
+
+        var cellStyle = {};
+
+        if (!this.state.value) {
+            // value = '-';
+            cellStyle.color = "transparent";
+        }
+
+        let handleClick = () => {
+            let newValue = this.state.value === 'O' ? 'X' : 'O';
+            this.setState({value: newValue});
+            console.log('new vale is ', newValue);
+            // window.alert("");
+            // if (t)
+        };
+
+        return (
+            <button className='cell' style={cellStyle} onClick={handleClick}>{this.state.value??'-'}</button>
+        );
+    }
+
 }
+
+/*let Cell = ({value}) => {
+
+    var cellStyle = {};
+
+    if (!value) {
+        value = '-';
+        cellStyle.color = "transparent";
+    }
+
+    let onclick = () => {
+        // window.alert("");
+        // if (t)
+    }
+
+    return (
+        <button className='cell' style={cellStyle} onClick={onclick}>{value}</button>
+    );
+}*/
 
 export default Cell;
