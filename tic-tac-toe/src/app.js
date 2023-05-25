4147b96fe7533db0071161c93dfdb88bc620c119
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-05-25 18:12:20 +0530

    day#7 update#2

--- tic-tac-toe/src/app.js
+++ tic-tac-toe/src/app.js
@@ -1,15 +1,31 @@
+import React from 'react';
 import AppHeader from './components/app-header.component';
 import Game from './components/game.component';
+import Clock from './components/clock.component';
 import './app.css';
 
-function App(){
+class App extends React.Component {
 
-    return (
+    state = {
+        showClock: true
+    }
+
+    handleToggle=()=>{
+        this.setState({showClock: !this.state.showClock});
+    }
+
+    render() {
+        return (
             <div className="app">
                 <AppHeader title="Tic Tac Toe" />
-                <Game/>
+                <div>
+                    { this.state.showClock && <Clock /> }
+                    <button onClick={this.handleToggle} >Toggle Clock</button>
+                </div>
+                <Game />
             </div>
         );
+    }
 }
 
 export default App;
